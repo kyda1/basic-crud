601b6489f2bb601549d70ba3e52e3eb9f78b5688
KY <139532518+kyda1@users.noreply.github.com>
2023-09-05 14:24:37 +0800

    Update index.php

--- crud/index.php
+++ crud/index.php
@@ -37,42 +37,86 @@ $result = mysqli_query($connection, $sql);
                 <th>Address</th>
                 <th>Actions</th>
             </tr>
-            <?php
-                while ($row = mysqli_fetch_assoc($result)) {
+
+        <?php
+            while ($row = mysqli_fetch_assoc($result)) {
                 echo "<tr>";
                 echo "<td>" . $row['fullname'] . "</td>";
                 echo "<td>" . $row['bday'] . "</td>";
                 echo "<td>" . $row['address'] . "</td>";
                 echo "<td>";
-                echo '<button class=""edit-btn">Edit</button></a>';   
+                echo '<button class="edit-btn" onclick="openEditModal(' . $row['id'] . ', \'' . $row['fullname'] . '\', \'' . $row['bday'] . '\', \'' . $row['address'] . '\')">Edit</button>';
                 echo '<button class="delete-btn"><a href="delete.php?id=' . $row['id'] . '">Delete</a></button>';
                 echo "</td>";
                 echo "</tr>";
-}
-?>
+            }
+        ?>
+
 
         </table>
     </div>
-    <h1>Edit Information</h1>
-        <div class="form">
-            <h2>Modify Info</h2>
-            <form method="post" action="update.php">
-                <?php
-                // Check if the expected parameters exist in $_GET
-                $id = isset($_GET['id']) ? $_GET['id'] : '';
-                $fullname = isset($_GET['fullname']) ? $_GET['fullname'] : '';
-                $bday = isset($_GET['bday']) ? $_GET['bday'] : '';
-                $address = isset($_GET['address']) ? $_GET['address'] : '';
-                ?>
-                <input type="hidden" name="id" value="<?php echo $id; ?>">
-                <input type="text" name="fullname" placeholder="Fullname" value="<?php echo $fullname; ?>">
-                <input type="date" name="bday" placeholder="Birthdate" value="<?php echo $bday; ?>">
-                <input type="text" name="address" placeholder="Address" value="<?php echo $address; ?>">
-                <button type="submit">Modify</button>
-            </form>
+    <div class="modal-bg" id="editModal">
+            <div class="form--update">
+            <div class="form">
+            <span class="close" id="closeModal">&times;</span>
+                <form method="post" action="update.php">
+                    <h2>Modify Information</h2>
+                    <?php
+                        $id = isset($_GET['id']) ? $_GET['id'] : '';
+                        $fullname = isset($_GET['fullname']) ? $_GET['fullname'] : '';
+                        $bday = isset($_GET['bday']) ? $_GET['bday'] : '';
+                        $address = isset($_GET['address']) ? $_GET['address'] : '';
+                    ?>
+                        <input type="hidden" name="id" value="<?php echo $id; ?>">
+                        <input type="text" name="fullname" placeholder="Fullname" value="<?php echo $fullname; ?>">
+                        <input type="date" name="bday" placeholder="Birthdate" value="<?php echo $bday; ?>">
+                        <input type="text" name="address" placeholder="Address" value="<?php echo $address; ?>">
+                    <button type="submit">Modify</button>
+                </form>
+            </div>
         </div>
+        </div>
+    </div>
 </body>
 </html>
 <?php
 mysqli_close($connection);
 ?>
+
+<script>
+function openEditModal(id) {
+    const editModal = document.getElementById('editModal');
+    editModal.style.display = 'block';
+
+}
+
+const closeModalButton = document.getElementById('closeModal');
+
+closeModalButton.addEventListener('click', () => {
+    const editModal = document.getElementById('editModal');
+    editModal.style.display = 'none';
+});
+
+window.addEventListener('click', (event) => {
+    const editModal = document.getElementById('editModal');
+    if (event.target === editModal) {
+        editModal.style.display = 'none';
+    }
+});
+
+function openEditModal(id, fullname, bday, address) {
+    const editModal = document.getElementById('editModal');
+    const idInput = editModal.querySelector('input[name="id"]');
+    const fullnameInput = editModal.querySelector('input[name="fullname"]');
+    const bdayInput = editModal.querySelector('input[name="bday"]');
+    const addressInput = editModal.querySelector('input[name="address"]');
+
+    idInput.value = id;
+    fullnameInput.value = fullname;
+    bdayInput.value = bday;
+    addressInput.value = address;
+
+    editModal.style.display = 'block';
+}
+
+</script>
